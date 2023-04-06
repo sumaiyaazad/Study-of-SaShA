5b1bc8dfc8717042dd09912e49c9d96fd62de926
Farhana100 <farhanakhanhp@gmail.com>
2023-04-06 15:06:16 +0600

    can start with attack models now ig ;-;

--- config.py
+++ config.py
@@ -11,5 +11,4 @@ MAX_ITER = 100
 # data loader
 
 COLD_START_THRESHOLD = 5 # to avoid cold start drop users with less than 5 ratings and items with less than 5 ratings [ref: https://link.springer.com/chapter/10.1007/978-3-030-49461-2_18]
-SAMPLE_FRAC = 0.01 # randomly sample 25% of the data [ref: https://link.springer.com/chapter/10.1007/978-3-030-49461-2_18]
-# SAMPLE_FRAC = 0.25 # randomly sample 25% of the data [ref: https://link.springer.com/chapter/10.1007/978-3-030-49461-2_18]
+SAMPLE_FRAC = 0.25 # randomly sample 25% of the data [ref: https://link.springer.com/chapter/10.1007/978-3-030-49461-2_18]
